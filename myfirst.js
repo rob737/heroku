f46dc42fc3e57c541e7f366851b2e02a18c43300
rob737 <robinsrivastava737@gmail.com>
2017-07-15 22:08:42 +0530

    Finally Heroku

---
+++ myfirst.js
@@ -0,0 +1,11 @@
+var http=require('http');
+var dt=require('./dateModule');
+
+http.createServer(function (req,res) {
+	res.writeHead(200, {'Content-Type':'text/html'});
+	res.write("Current date:\n"+ dt.myDateTime());
+	res.write(req.url);
+	res.end();
+	console.log('Having fun');
+}).listen(8080);
+
